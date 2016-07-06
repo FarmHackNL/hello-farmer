37b583a4de85669bc1cc424fc4a8e83f089dfafa
wvengen <dev-voko@willem.engen.nl>
2016-07-06 02:11:09 +0200

    Fix start animation on production (now really)

--- prototype/src/loadanim.js
+++ prototype/src/loadanim.js
@@ -1,20 +1,26 @@
 import $ from 'jquery';
 
+let animationStarted = false;
+
 // initial zoom animation after map load
 export default function(map, position, layers) {
   let loading = layers.length;
   layers.forEach((layer) => {
     layer.on('load', () => {
       loading -= 1;
-      if (loading === 0) {
+      if (loading === 0 && !animationStarted) {
         startAnimation(map, position);
       }
     });
   });
   $('#intro-container').fadeIn('slow');
+  // but also on timeout, in case load events were missed (e.g. already loaded)
+  // @todo proper implementation catching load events always
+  setTimeout(() => !animationStarted && startAnimation(map, position), 800);
 }
 
 function startAnimation(map, position) {
+  animationStarted = true;
   $('#intro-container').fadeIn('slow', function() {
     setTimeout(function() {
       $('#intro > div').fadeOut('slow');
